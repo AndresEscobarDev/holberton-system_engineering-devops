b107151dcded104fef31589e27a45edcc41b42be
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-08-03 15:17:15 +0000

    Mandatory tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in /tmp named holberton
+file {'/tmp/holberton':
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I Love Puppet',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Kills a process named killmenow.
+exec {'killmenow':
+    path => '/usr/bin',
+    command => 'pkill killmenow',
+    provider => 'shell',
+    returns => [0, 1],
+}
