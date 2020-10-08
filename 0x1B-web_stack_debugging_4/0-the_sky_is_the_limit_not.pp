a2415c94ce9990be0f2caa1e20c250333fb1490a
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-10-08 06:34:57 -0500

    Mandatory task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Fix the request of nginx server
+exec { 'fix-requests':
+    command => "sed -i 's/worker_processes 4;/worker_processes 8;/g' /etc/nginx/nginx.conf; sudo service nginx restart"
+    path    => ['/bin', '/usr/bin']
+}
